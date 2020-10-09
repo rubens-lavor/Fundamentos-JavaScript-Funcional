5bd99a46589462c6bb5a4d75c10878b9234648ae
rubens-lavor <haryson07@gmail.com>
2020-10-09 16:35:33 -0300

    first class function

---
+++ functional/first-class-function.js
@@ -0,0 +1,12 @@
+/**
+ * diz-se que uma linguagem de programação tem
+ * Função de primeira classe quando funçoes nessa
+ * linguagem são tratadas como qualquer outra
+ * variável
+ */
+
+const add = function (a, b) {
+    return a + b
+}
+
+console.log(add(10, 20));
