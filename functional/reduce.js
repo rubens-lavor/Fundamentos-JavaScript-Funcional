7762acc89f5e0972cce049d20e6953f09fae5fcf
rubens-lavor <haryson07@gmail.com>
2020-10-11 19:58:05 -0300

    media com reduce

--- functional/reduce.js
+++ functional/reduce.js
@@ -4,3 +4,15 @@ const sum = (total, elem) => total + elem
 const total = numbers.reduce(sum,10)
 
 console.log(total);
+
+const avg = (acc, elem, i, array) => {
+    if (i === array.length - 1){
+        return (acc + elem) / array.length
+    }else {
+        return acc + elem
+    }
+}
+
+const resul = numbers.reduce(avg)
+
+console.log(resul);
