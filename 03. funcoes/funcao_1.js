6cc5279ce8497b6de8db3b6969612e25e32eabc7
rubens-lavor <haryson07@gmail.com>
2020-10-12 19:21:28 -0300

    função 1

---
+++ 03. funcoes/funcao_1.js
@@ -0,0 +1,20 @@
+//function declaration
+function bomDia() {
+    console.log("Bom dia");
+}
+bomDia()
+
+
+//funtion expression
+const boaTarde = function () {
+    console.log("boa tarde");
+}
+boaTarde()
+
+
+function somar(a,b){
+    return a + b
+}
+
+const result = somar(3,4)
+console.log(result);
