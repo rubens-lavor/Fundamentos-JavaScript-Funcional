d20c2499efc337cd7939bfc414e9e859f53dfbc4
rubens-lavor <haryson07@gmail.com>
2020-10-13 19:22:04 -0300

    arrow function

--- 03. funcoes/basico_3.js
+++ 03. funcoes/basico_3.js
@@ -6,3 +6,26 @@ felizNatal()
 const saudacao = nome => `Fala ${nome}, blz?`
 
 console.log(saudacao("mano"));
+
+const somar = (...numeros) => {
+    let total = 0
+    for (let n of numeros) {
+        total += n
+    }
+    return total
+}
+
+console.log(somar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+console.log(somar(1, 2, 3, 4, 5))
+
+
+const potencia = base => exp => Math.pow(base, exp)
+console.log(potencia(2)(8));
+
+
+// this
+Array.prototype.ultimo = function () {
+    console.log(this[this.length - 1]);
+}
+const numeros = [1,2,3]
+numeros.ultimo()
