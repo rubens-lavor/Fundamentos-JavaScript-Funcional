3e19274272175c217ab5f7b1e0c6c778fb56f6d0
rubens-lavor <haryson07@gmail.com>
2020-10-14 20:14:55 -0300

    map

--- 03. funcoes/callback_3.js
+++ 03. funcoes/callback_3.js
@@ -10,3 +10,17 @@ const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
 const primeiraLetra = texto => texto[0]
 const letras = nomes.map(primeiraLetra)
 console.log(nomes, letras)
+
+const carrinho = [
+    {nome:'caneta', qtde: 10, preco: 7.90},
+    {nome:'impressora', qtde: 0, preco: 650.00},
+    {nome:'caderno', qtde: 4, preco: 27.90},
+    {nome:'lapis', qtde: 3, preco: 5.78},
+    {nome:'tesoura', qtde: 1, preco: 5.99},
+]
+
+const getNome = item => item.nome
+const getTotal = item => item.preco * item.qtde
+
+console.log(carrinho.map(getNome))
+console.log(carrinho.map(getTotal))
