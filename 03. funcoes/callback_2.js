a3d9644f41ef0af56380782186ab576600193e41
rubens-lavor <haryson07@gmail.com>
2020-10-13 21:11:33 -0300

    aula de callback finalizada

--- 03. funcoes/callback_2.js
+++ 03. funcoes/callback_2.js
@@ -7,6 +7,12 @@ function exibirConteudo(_, conteudo) {
     console.log(conteudo.toString());
 }
 
-fs.readFile(caminho,{}, exibirConteudo)
-fs.readFile(caminho, (_, conteudo) => console.log(conteudo.toString()) )
+console.log("inicio..")
+fs.readFile(caminho, {}, exibirConteudo)
+fs.readFile(caminho, (_, conteudo) => console.log(conteudo.toString()))
+console.log("fim..")
 
+console.log("inicio sync..")
+const conteudo = fs.readFileSync(caminho)
+console.log(conteudo.toString())
+console.log("fim sync..")
