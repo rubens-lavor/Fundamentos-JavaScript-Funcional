2ddba3138fc28c817ad192340f8cdede05ee80e9
rubens-lavor <haryson07@gmail.com>
2020-10-16 21:19:51 -0300

    filter

---
+++ 03. funcoes/callback_4.js
@@ -0,0 +1,24 @@
+const carrinho = [
+    { nome: 'caneta', qtde: 10, preco: 7.90 },
+    { nome: 'impressora', qtde: 0, preco: 650.00 },
+    { nome: 'caderno', qtde: 4, preco: 27.90 },
+    { nome: 'lapis', qtde: 3, preco: 5.78 },
+    { nome: 'tesoura', qtde: 1, preco: 5.99 },
+]
+
+const qtdeMaiorQueZero = item => item.qtde > 1
+const getNome = item => item.nome
+const qtdeGrande = item => item.qtde >= 1000
+
+
+const itensValidos = carrinho.filter(qtdeMaiorQueZero)
+
+const NomesitensValidos = carrinho
+    .filter(qtdeMaiorQueZero)
+    .map(getNome)
+    ;
+const grandeQtde = carrinho.filter(qtdeGrande)
+
+console.log(grandeQtde);
+console.log(itensValidos);
+console.log(NomesitensValidos);
