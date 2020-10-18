77808216bcc6009d21bf296a413001fd3261f65d
rubens-lavor <haryson07@gmail.com>
2020-10-17 23:01:26 -0300

    update

--- 03. funcoes/callback_5.js
+++ 03. funcoes/callback_5.js
@@ -2,7 +2,7 @@
 
 const carrinho = [
     { nome: 'caneta', qtde: 10, preco: 7.90 },
-    { nome: 'impressora', qtde: 0, preco: 650.00 },
+    { nome: 'impressora', qtde: 1, preco: 650.50 },
     { nome: 'caderno', qtde: 4, preco: 27.90 },
     { nome: 'lapis', qtde: 3, preco: 5.78 },
     { nome: 'tesoura', qtde: 1, preco: 5.99 },
@@ -11,6 +11,18 @@ const carrinho = [
 
 const getTotal = item => item.qtde * item.preco
 
-const totalGeral = carrinho.map(getTotal)
+
+const totais = carrinho.map(getTotal)
+
+
+console.log(totais);
+
+const somar = (acc, el) => {
+    console.log(acc + " + " + el);
+    return acc + el
+}
+
+const totalGeral = totais.reduce(somar)
+
 
 console.log(totalGeral);
