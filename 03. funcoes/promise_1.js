bceffd6bf818f364559683a53d08040036ffab6b
rubens-lavor <haryson07@gmail.com>
2020-10-18 23:14:24 -0300

    .then

--- 03. funcoes/promise_1.js
+++ 03. funcoes/promise_1.js
@@ -1,4 +1,12 @@
 let a = 1
 console.log(a);
 
-let p = new Promise()
+let p = new Promise(function(cumprirPromessa) {
+    cumprirPromessa(["ana","josé","edu"])
+})
+
+p
+    .then(valor => valor[0])
+    .then(primeiro => primeiro[0])
+    .then(letra => letra.toUpperCase())
+    .then(letraMaiuscula => console.log(letraMaiuscula))
