2e1c2f61245dd177e47b5fe7f52c996c3e637df7
rubens-lavor <haryson07@gmail.com>
2020-10-27 21:30:44 -0300

    finalizado ex 2

--- 03. funcoes/promise_2.js
+++ 03. funcoes/promise_2.js
@@ -1,4 +1,24 @@
-
+/*
 setTimeout(() => {
-    console.log("executando callback");
+    console.log("executando callback...");
+    setTimeout(() => {
+        console.log("executando callback...");
+        setTimeout(() => {
+            console.log("executando callback...");
+        }, 2000)
+    }, 2000)
 }, 2000);
+*/
+
+function espararPor(tempo = 2000){
+    return new Promise(function(resolve) {
+        setTimeout(() => {
+            console.log("executando promise...");
+            resolve()
+        }, tempo);
+    })
+}
+
+espararPor()
+    .then(() => espararPor())
+    .then(espararPor)
