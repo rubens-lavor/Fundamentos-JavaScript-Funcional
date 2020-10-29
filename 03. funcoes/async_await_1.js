8dbfff90c94044d0da154b15ac9e2697581815be
rubens-lavor <haryson07@gmail.com>
2020-10-29 18:33:30 -0300

    async/await 1

--- 03. funcoes/async_await_1.js
+++ 03. funcoes/async_await_1.js
@@ -6,9 +6,38 @@ function esperarPor(tempo = 2000){
     })
 }
 
+/*
+
 esperarPor(2000)
     .then(()=> console.log("executando promise 1..."))
     .then(esperarPor)
     .then(()=> console.log("executando promise 2..."))
     .then(esperarPor)
     .then(()=> console.log("executando promise 3..."))
+*/
+
+function retornarValor(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(10)
+        }, 5000);
+    })
+}
+
+
+async function executar(){
+
+    let valor = await retornarValor()
+
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor}...`);
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 1}...`);
+    await esperarPor(1500)
+    console.log(`Async/Await ${valor + 2}...`);
+
+
+    return valor + 3
+}
+
+executar().then(console.log)
