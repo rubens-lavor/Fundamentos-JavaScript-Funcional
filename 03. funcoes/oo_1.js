8775f8b0d7496ff45a1c8c371c7ff7d98488b320
rubens-lavor <haryson07@gmail.com>
2020-10-29 19:06:09 -0300

    oo 1 finalizada

---
+++ 03. funcoes/oo_1.js
@@ -0,0 +1,15 @@
+function Produto (nome, preco, desc = 0.15){
+    this.nome = nome
+    this.preco = preco
+    this.desc = desc
+
+    this.precoFinal = function() {
+        return this.preco * (1-this.desc)
+    }
+}
+
+const p1 = new Produto('Caneta', 2.34)
+console.log(p1.nome);
+const p2 = new Produto('Geladeira', 12.30)
+console.log(p2.preco);
+console.log(p2.precoFinal());
