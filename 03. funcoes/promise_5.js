815e7dead83f07cd003f9283656f9887a5d145ec
rubens-lavor <haryson07@gmail.com>
2020-10-28 21:32:24 -0300

    try catch

--- 03. funcoes/promise_5.js
+++ 03. funcoes/promise_5.js
@@ -3,10 +3,21 @@ const { resolve } = require("path");
 
 function funcionarOuNao(valor, chanceErro) {
     return new Promise((resolve,reject)=>{
-        if (Math.random() < chanceErro) {
-            reject('Ocorreu um erro!')
-        }else{
-            resolve(valor)
+        try {
+            con.log('tempo')
+            if (Math.random() < chanceErro) {
+                reject('Ocorreu um erro!')
+            }else{
+                resolve(valor)
+            }
+            
+        } catch (e) {
+            reject(e)
         }
+        
     })
 }
+
+funcionarOuNao('Testando...', 0.5)
+    .then(v => console.log(`Valor ${v}`))
+    .catch(err => console.log(`Erro: ${err}`))
