fabe7c1d5b26a47dafc979eeb3bbc8d9d5a6a397
rubens-lavor <haryson07@gmail.com>
2020-10-29 19:24:34 -0300

    oo 2

--- 03. funcoes/oo_2.js
+++ 03. funcoes/oo_2.js
@@ -6,7 +6,34 @@ class Produto {
         this.desc = desc
     }
 
-    precoFinal(){
+    get nome(){
+        return `Produto: ${this._nome}`
+    }
+
+    set nome(novoNome){
+        this._nome = novoNome.toUpperCase()
+    }
+
+    get preco(){
+        return this._preco
+    }
+
+    set preco(novoPreco){
+        if(novoPreco >=0 ){
+            this._preco = novoPreco
+        }
+    }
+
+    get precoFinal(){
         return this.preco * (1-this.desc)
     }
 }
+
+const p1 = new Produto('Caneta', 2.34)
+//p1.nome = "caneta"
+console.log(p1.nome);
+console.log(p1.preco);
+
+const p2 = new Produto('Geladeira', 10000, 0.8)
+console.log(p2.preco);
+console.log(p2.precoFinal);
