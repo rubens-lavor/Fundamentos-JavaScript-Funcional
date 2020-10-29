f7c8af81a5c55e1c5732999112bf0a6a42b0a376
rubens-lavor <haryson07@gmail.com>
2020-10-29 18:16:56 -0300

    update

--- 03. funcoes/async_await_1.js
+++ 03. funcoes/async_await_1.js
@@ -1,8 +1,14 @@
-function espararPor(tempo = 2000){
+function esperarPor(tempo = 2000){
     return new Promise(function(resolve) {
         setTimeout(() => {
-            console.log("executando promise...");
             resolve()
         }, tempo);
     })
 }
+
+esperarPor(2000)
+    .then(()=> console.log("executando promise 1..."))
+    .then(esperarPor)
+    .then(()=> console.log("executando promise 2..."))
+    .then(esperarPor)
+    .then(()=> console.log("executando promise 3..."))
