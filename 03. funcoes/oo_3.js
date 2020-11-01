4891874b68d3821908817ddccc9a1626ad81bf4f
rubens-lavor <haryson07@gmail.com>
2020-10-31 23:29:19 -0300

    oo_ 3 finalizada

--- 03. funcoes/oo_3.js
+++ 03. funcoes/oo_3.js
@@ -16,6 +16,11 @@ Produto.prototype.log = function () {
 Object.defineProperty(Produto.prototype, 'desc',{
     get: function(){
         return this._desc
+    }, 
+    set: function(novoDesc) {
+        if (novoDesc >= 0 && novoDesc <=1) {
+            this._desc = novoDesc
+        }
     }
 })
 
@@ -32,6 +37,6 @@ console.log(p1.nome);
 const p2 = new Produto('Geladeira', 12.30)
 console.log(p2.preco);
 console.log(p2.precoFinal());
-
+p2.desc = 0.99
 console.log(p2.desc);
 console.log(p2.descString);
