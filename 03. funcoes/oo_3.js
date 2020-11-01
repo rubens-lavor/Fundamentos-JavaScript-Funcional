1f4aa8b3f95145744f7d8ce720d50b31d530bed6
rubens-lavor <haryson07@gmail.com>
2020-10-31 22:42:07 -0300

    prototype

---
+++ 03. funcoes/oo_3.js
@@ -0,0 +1,22 @@
+//função construtora
+function Produto (nome, preco, desc = 0.15){
+    this.nome = nome
+    this.preco = preco
+    this.desc = desc
+
+    this.precoFinal = function() {
+        return this.preco * (1-this.desc)
+    }
+}
+
+Produto.prototype.log = function () {
+    console.log(`Nome: ${this.nome} - Preço: R$ ${this.preco}`);
+}
+
+const p1 = new Produto('Caneta', 2.34)
+p1.log()
+
+console.log(p1.nome);
+const p2 = new Produto('Geladeira', 12.30)
+console.log(p2.preco);
+console.log(p2.precoFinal());
