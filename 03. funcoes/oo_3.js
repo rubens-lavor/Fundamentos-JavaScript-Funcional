608a521fb18c2f0f8fe6001157eb2ccd8b7bc763
rubens-lavor <haryson07@gmail.com>
2020-10-31 23:23:10 -0300

    get

--- 03. funcoes/oo_3.js
+++ 03. funcoes/oo_3.js
@@ -2,16 +2,28 @@
 function Produto (nome, preco, desc = 0.15){
     this.nome = nome
     this.preco = preco
-    this.desc = desc
+    this._desc = desc
 
     this.precoFinal = function() {
-        return this.preco * (1-this.desc)
+        return this.preco * (1-this._desc)
     }
 }
 
 Produto.prototype.log = function () {
     console.log(`Nome: ${this.nome} - Preço: R$ ${this.preco}`);
 }
+
+Object.defineProperty(Produto.prototype, 'desc',{
+    get: function(){
+        return this._desc
+    }
+})
+
+Object.defineProperty(Produto.prototype, 'descString',{
+    get: function(){
+        return `${this._desc * 100}% de desconto`
+    }
+})
 
 const p1 = new Produto('Caneta', 2.34)
 p1.log()
@@ -20,3 +32,6 @@ console.log(p1.nome);
 const p2 = new Produto('Geladeira', 12.30)
 console.log(p2.preco);
 console.log(p2.precoFinal());
+
+console.log(p2.desc);
+console.log(p2.descString);
